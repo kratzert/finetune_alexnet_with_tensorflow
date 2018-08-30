e08b8e62e8e36a66aaefcce850e17baefd2f7e63
wogong <wogong38@gmail.com>
2018-08-30 20:08:04 +0800

    compatibility update, port data load code to tf.data, etc.

--- datagenerator.py
+++ datagenerator.py
@@ -7,7 +7,7 @@
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.contrib.data import Dataset
+from tensorflow.data import Dataset
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework.ops import convert_to_tensor
 
@@ -67,12 +67,14 @@ class ImageDataGenerator(object):
 
         # distinguish between train/infer. when calling the parsing functions
         if mode == 'training':
-            data = data.map(self._parse_function_train, num_threads=8,
-                      output_buffer_size=100*batch_size)
+            data = data.map(
+                self._parse_function_train,
+                num_parallel_calls=8).prefetch(100 * batch_size)
 
         elif mode == 'inference':
-            data = data.map(self._parse_function_inference, num_threads=8,
-                      output_buffer_size=100*batch_size)
+            data = data.map(
+                self._parse_function_inference,
+                num_parallel_calls=8).prefetch(100 * batch_size)
 
         else:
             raise ValueError("Invalid mode '%s'." % (mode))
